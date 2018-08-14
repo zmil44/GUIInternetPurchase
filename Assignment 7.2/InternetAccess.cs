a0c8745c77bcdf768d8c9b273d4683ed68eb126f
zmil44 <zachary.j.miller@outlook.com>
2018-08-14 12:37:38 -0400

    initial commit

---
+++ Assignment 7.2/InternetAccess.cs
@@ -0,0 +1,29 @@
+﻿/*Zachary Miller
+ * 12/20/2017
+ * assignment 7.2
+ * The purpose of this program is display message boxes asking if you want internet or not and if you want unlimited or limited internet and then displays your final price
+ * InternetAccess.cs
+ * Bellevue University
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_7._2
+{
+    static class InternetAccess
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MessageBox());
+        }
+    }
+}
---
+++ Assignment 7.2/MessageBox.cs
@@ -0,0 +1,55 @@
+﻿/*Zachary Miller
+ * 12/20/2017
+ * assignment 7.2
+ * This contains the code to display the message boxes
+ * MessageBox.cs
+ * Bellevue University
+ */
+ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_7._2
+{
+    public partial class MessageBox : Form
+    {
+        public MessageBox()
+        {
+            
+            InitializeComponent();
+            String message = "Would you like Internet Access?";
+            String caption = "Internet";
+            DialogResult result = System.Windows.Forms.MessageBox.Show(message, caption, MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                message = "If you would like Unlimited access ($19.95/month), press yes, if you would like Limited internet access ($10.95/month), press no.";
+                result = System.Windows.Forms.MessageBox.Show(message, caption, MessageBoxButtons.YesNo);
+
+                if(result==DialogResult.Yes)
+                {
+                    message = "Your total price is $19.95";
+                    System.Windows.Forms.MessageBox.Show(message, caption, MessageBoxButtons.OK);
+                }
+                else if(result==DialogResult.No)
+                {
+                    message = "Your total price is $10.95";
+                    System.Windows.Forms.MessageBox.Show(message, caption, MessageBoxButtons.OK);
+                }
+            }
+
+            else
+            {
+                
+                message = ("Your total price is $0");
+                System.Windows.Forms.MessageBox.Show(message, caption, MessageBoxButtons.OK);
+            }
+        }
+    }
+}
